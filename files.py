900575646acc368396577e968a0baa36f9f4ddca
mrcod3r <mrcod3r.ir@gmail.com>
2021-11-11 13:11:04 +0330

    complete file

--- files.py
+++ files.py
@@ -1,1 +1,28 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+from typing import Text
+
+
+myFile = open('myfile.txt','w') # w mean write mode
+
+# Get some info
+print('Name: ',myFile.name)
+print('Is Closed : ',myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Pyhton')
+myFile.write(' and Javascript')
+myFile.close()
+print('Is Closed : ',myFile.closed)
+
+# Append to file
+myFile = open('myfile.txt','a') # a mean append mode
+myFile.write(' I also like PHP')
+myFile.close()
+
+# Read from file
+myFile = open('myfile.txt','r+') # r mean read mode
+text = myFile.read(100)
+print(text)
